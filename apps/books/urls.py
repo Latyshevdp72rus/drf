35e182e260df02845df6c91e717499ed42005a0b
Latyshevdp <Latyshevdp.contacts@yandex.ru>
2022-08-29 21:04:13 +0500

    generic

--- apps/books/urls.py
+++ apps/books/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path, include
-from apps.books.views import PublishingHouseAPIView, PublishingHoueseCreateAPIView, AuthorAPIView, AuthorCreateAPIView,BookAPIView,BookCreateAPIView
+from apps.books.views import PubViewDetail,PublishingHouseAction
 
 urlpatterns = [
-    path('publishing_house/<int:pk>', PublishingHouseAPIView.as_view(), ),
-    path('publishing_house', PublishingHoueseCreateAPIView.as_view(), ),
-    path('Author/<int:pk>', AuthorAPIView.as_view(), ),
-    path('Author', AuthorCreateAPIView.as_view(), ),
-    path('Book/<int:pk>', BookAPIView.as_view(), ),
-    path('Book', BookCreateAPIView.as_view(), ),
-
+    path('publishing_house/', PublishingHouseAction.as_view(),
+    path('publishing_house/<int:pk>', PubViewDetail.as_view())),
 ]
+
+    # PublishingHouseAction
+# PublishingHoueseCreateAPIView, AuthorAPIView, AuthorCreateAPIView,BookAPIView,BookCreateAPIView
+    # path('publishing_house/', PublishingHouseAction.as_view(), ),
+    # path('publishing_house', PublishingHoueseCreateAPIView.as_view(), ),
+    # path('Author/<int:pk>', AuthorAPIView.as_view(), ),
+    # path('Author', AuthorCreateAPIView.as_view(), ),
+    # path('Book/<int:pk>', BookAPIView.as_view(), ),
+    # path('Book', BookCreateAPIView.as_view(), ),
+
--- apps/books/views.py
+++ apps/books/views.py
@@ -1,96 +1,152 @@
-from rest_framework import status
-from rest_framework.views import APIView
 from rest_framework.response import Response
-from apps.books.models import PublishingHouse,Author,Book
-from apps.books.serialzers import PublishingHouseSerializer,AuthorSerializer,BookSerializer
 
-
-class PublishingHouseAPIView(APIView):
-    def get(self, request, pk):
-        queryset = PublishingHouse.objects.filter(pk=pk)
-        serializer = PublishingHouseSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        publishing_house =PublishingHouse.objects.get(pk=pk)
-        serializer = PublishingHouseSerializer(instance=publishing_house,data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self,request,pk):
-        publishing_house = PublishingHouse.objects.get(pk=pk)
-        publishing_house.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+from apps.books.models import PublishingHouse
+from apps.books.serialzers import PublishingHouseSerializer
+from rest_framework.mixins import CreateModelMixin
+from rest_framework.generics import ListAPIView, RetrieveAPIView,CreateAPIView,RetrieveUpdateDestroyAPIView
 
 
 
-class PublishingHoueseCreateAPIView(APIView):
-    # разрешаем запросы
-    def post(self, request):
-        serializer = PublishingHouseSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-""""""
-class AuthorAPIView(APIView):
-    def get(self, request, pk):
-        queryset = Author.objects.filter(pk=pk)
-        serializer = AuthorSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        author = Author.objects.get(pk=pk)
-        serializer = AuthorSerializer(instance=author, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        author = Author.objects.get(pk=pk)
-        author.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class PublishingHouseAction(CreateAPIView):
+    queryset = PublishingHouse.objects.all()
+    serializer_class = PublishingHouseSerializer
 
 
-class AuthorCreateAPIView(APIView):
-    # разрешаем запросы
-    def post(self, request):
-        serializer = AuthorSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class PubViewDetail(RetrieveUpdateDestroyAPIView):
+    queryset = PublishingHouse.objects.all()
+    serializer_class = PublishingHouseSerializer
 
 
-class BookAPIView(APIView):
-    def get(self, request, pk):
-        queryset = Book.objects.filter(pk=pk)
-        serializer = BookSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        book = Book.objects.get(pk=pk)
-        serializer = BookSerializer(instance=book, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        book = Book.objects.get(pk=pk)
-        book.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class PublishingHouseAction(CreateModelMixin, ListAPIView):
+    queryset = PublishingHouse.objects.all()
+    serializer_class = PublishingHouseSerializer
 
 
-class BookCreateAPIView(APIView):
-    # разрешаем запросы
-    def post(self, request):
-        serializer = BookSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from rest_framework import status
+
+
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# ,Author,Book
+# ,AuthorSerializer,BookSerializer
+
+
+#     def get_queryset(self):
+#         pass
+        # 1 Способ
+        # queryset = PublishingHouse.objects.exclude(pk=1)
+        # return PublishingHouse.objects.exclude(pk=1)
+        # 2 способ
+        # filter_value = self.request.get('value')
+        # return PublishingHouse.objects.exclude(pk=1)
+
+
+
+# class PublishingHouseAPIView(APIView):
+#     def get(self, request, pk):
+#         queryset = PublishingHouse.objects.filter(pk=pk)
+#         serializer = PublishingHouseSerializer(queryset, many=True)
+#         return Response(serializer.data)
+#
+#     def put(self, request, pk):
+#         publishing_house =PublishingHouse.objects.get(pk=pk)
+#         serializer = PublishingHouseSerializer(instance=publishing_house,data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def delete(self,request,pk):
+#         publishing_house = PublishingHouse.objects.get(pk=pk)
+#         publishing_house.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+#
+#
+#
+# class PublishingHoueseCreateAPIView(APIView):
+#     # разрешаем запросы
+#     def post(self, request):
+#         serializer = PublishingHouseSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+# """"""
+# class AuthorAPIView(APIView):
+#     def get(self, request, pk):
+#         queryset = Author.objects.filter(pk=pk)
+#         serializer = AuthorSerializer(queryset, many=True)
+#         return Response(serializer.data)
+#
+#     def put(self, request, pk):
+#         author = Author.objects.get(pk=pk)
+#         serializer = AuthorSerializer(instance=author, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def delete(self, request, pk):
+#         author = Author.objects.get(pk=pk)
+#         author.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+#
+#
+# class AuthorCreateAPIView(APIView):
+#     # разрешаем запросы
+#     def post(self, request):
+#         serializer = AuthorSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#
+# class BookAPIView(APIView):
+#     def get(self, request, pk):
+#         queryset = Book.objects.filter(pk=pk)
+#         serializer = BookSerializer(queryset, many=True)
+#         return Response(serializer.data)
+#
+#     def put(self, request, pk):
+#         book = Book.objects.get(pk=pk)
+#         serializer = BookSerializer(instance=book, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def delete(self, request, pk):
+#         book = Book.objects.get(pk=pk)
+#         book.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+#
+#
+# class BookCreateAPIView(APIView):
+#     # разрешаем запросы
+#     def post(self, request):
+#         serializer = BookSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
