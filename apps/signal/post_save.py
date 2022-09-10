9a926013e8cd6c74f94f831432d2f87bcf036016
Latyshevdp <Latyshevdp.contacts@yandex.ru>
2022-09-10 16:13:12 +0500

    signals

--- apps/signal/post_save.py
+++ apps/signal/post_save.py
@@ -3,19 +3,19 @@ from django.dispatch import receiver
 from apps.books.models import Book, Author, PublishingHouse
 import logging
 
-
+# КНИГИ
 @receiver(post_save, sender=Book)
 def book_save_log(sender, instance, created, **kwargs):
     if created:
         logging.warning(f"В таблице Book, появилась новая запись  {instance.book_name}")
 
-
+# Публикация
 @receiver(post_save, sender=PublishingHouse)
 def publish_house_save_log(sender, instance, created, **kwargs):
     if created:
         logging.warning(f"В таблице PublishingHouse, появилась новая запись  {instance.publishing_house_name} ")
 
-
+# Авторы
 @receiver(post_save, sender=Author)
 def author_save_log(sender, instance, created, **kwargs):
     if created:
--- apps/signal/pre_delete.py
+++ apps/signal/pre_delete.py
@@ -3,19 +3,19 @@ from django.dispatch import receiver
 from apps.books.models import Book, Author, PublishingHouse
 import logging
 
-
+# КНИГИ
 @receiver(pre_delete, sender=Book)
 def delete_book_log(sender, instance, deleted, **kwargs):
     if deleted:
         logging.warning(f"В таблице Book, произошло удаление записи: {instance.book_name} ")
 
-
+# Публикация
 @receiver(pre_delete, sender=PublishingHouse)
 def delete_publishing_house_log(sender, instance, deleted, **kwargs):
     if deleted:
         logging.warning(f"В таблице PublishingHouse, произошло удаление записи: {instance.publishing_house_name}")
 
-
+# Авторыы
 @receiver(pre_delete, sender=Author)
 def delete_author_log(sender, instance, deleted, **kwargs):
     if deleted:
