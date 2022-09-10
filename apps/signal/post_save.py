41008a1eee914d53afa38e3e8b918816ba0fa891
Latyshevdp <Latyshevdp.contacts@yandex.ru>
2022-09-10 15:29:19 +0500

    signals

--- apps/signal/post_save.py
+++ apps/signal/post_save.py
@@ -7,18 +7,17 @@ import logging
 @receiver(post_save, sender=Book)
 def log_book_save(sender, instance, created, **kwargs):
     if created:
-        logging.warning(f"В таблице Book, появилась новая запись  {instance.book_name}  и она успешно создана")
+        logging.warning(f"В таблице Book, появилась новая запись  {instance.book_name}")
 
 
 @receiver(post_save, sender=PublishingHouse)
 def log_publish_house_save(sender, instance, created, **kwargs):
     if created:
-        logging.warning(f"В таблице PublishingHouse, появилась новая запись  {instance.publishing_house_name} \
-            и она успешно создана")
+        logging.warning(f"В таблице PublishingHouse, появилась новая запись  {instance.publishing_house_name} ")
 
 
 @receiver(post_save, sender=Author)
 def log_author_save(sender, instance, created, **kwargs):
     if created:
-        logging.warning(f"В таблице Author, появилась новая запись {instance.first_name} {instance.last_name} {instance.fathername} и она успешно создана")
+        logging.warning(f"В таблице Author, появилась новая запись {instance.first_name} {instance.last_name} {instance.fathername}")
 
--- apps/signal/pre_delete.py
+++ apps/signal/pre_delete.py
@@ -1,14 +1,19 @@
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
-from apps.books.models import Book, Author
+from apps.books.models import Book, Author,PublishingHouse
 import logging
 
 
 @receiver(pre_delete, sender=Book)
 def delete_book_log(sender, instance, **kwargs):
-    logging.warning(f"Произошло удаление книги:{sender} Название книги {instance.book_name} ")
+    logging.warning(f"В таблице Book, произошло удаление записи:{sender} Название книги {instance.book_name} ")
+
+
+@receiver(pre_delete, sender=PublishingHouse)
+def delete_author_log(sender, instance, **kwargs):
+    logging.warning(f"В таблице PublishingHouse, произошло удаление записи: {instance.publishing_house_name}")
 
 
 @receiver(pre_delete, sender=Author)
 def delete_author_log(sender, instance, **kwargs):
-    logging.warning(f"Произошло удаление книги:{sender} Название книги  {instance.first_name} {instance.last_name} {instance.fathername}  ")
+    logging.warning(f"В таблице Author, произошло удаление записи:  {instance.first_name} {instance.last_name} {instance.fathername}")
