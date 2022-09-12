4835e088f8de61dc42adb390cef6ebbf8554c4b4
Latyshevdp <Latyshevdp.contacts@yandex.ru>
2022-09-12 18:52:42 +0500

    acount_auth

--- apps/acount/serializer.py
+++ apps/acount/serializer.py
@@ -3,9 +3,21 @@ from apps.acount.models import User
 
 
 class RegistrationUserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+
+    )
+    username =serializers.CharField(
+
+    )
+    password = serializers.CharField(
+        max_length=128,
+        min_length=8,
+        write_only=True
+    )
+
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['email', 'username', 'password']
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
