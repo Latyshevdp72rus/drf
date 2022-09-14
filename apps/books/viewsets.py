950488275d75fa0078ab3033f4df2868211f5630
Latyshevdp <Latyshevdp.contacts@yandex.ru>
2022-09-14 20:49:16 +0500

    task  celery

--- apps/books/viewsets.py
+++ apps/books/viewsets.py
@@ -11,18 +11,6 @@ class PublishingHouseViewSet(viewsets.ModelViewSet):
     queryset = PublishingHouse.objects.all()
     serializer_class = PublishingHouseSerializer
 
-
-class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-
-
-class AuthorViewSet(viewsets.ModelViewSet):
-    queryset = Author.objects.all()
-    serializer_class = AuthorSerializer
-
-
-class PublishHouseAction(ViewSet):
     def list(self, request):
         queryset = PublishingHouse.objects.all()
         serializer = PublishingHouseSerializer(queryset, many=True)
@@ -34,6 +22,32 @@ class PublishHouseAction(ViewSet):
         serializer = PublishingHouseSerializer(handler, many=True)
         return Response(serializer.data)
 
-    @inform_new
     def create(self, request):
-        pass
+        inform_new.delay()
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+#
+# class PublishHouseAction(ViewSet):
+#     def list(self, request):
+#         queryset = PublishingHouse.objects.all()
+#         serializer = PublishingHouseSerializer(queryset, many=True)
+#         return Response(serializer.data)
+#
+#     def retrieve(self, request, pk):
+#         queryset = PublishingHouse.objects.all()
+#         handler = get_object_or_404(queryset, pk=pk)
+#         serializer = PublishingHouseSerializer(handler, many=True)
+#         return Response(serializer.data)
+#
+#     @inform_new
+#     def create(self, request):
+#         pass
